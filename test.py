b6b028365aeea5ec43d786a2e00058707ac73464
Manh <manhgu3110@gmail.com>
2026-04-30 23:33:03 +0700

    Fix .txt save all images

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 import torch
+import pandas as pd
+
+df = pd.read_excel("./data/BTXRD/dataset.xlsx")
+# Chọn các cột feature cần kiểm tra
+lesion_cols = df.loc[:, "osteochondroma":"other mt"]
+df['max_multiple'] = multi_features = lesion_cols.sum(axis = 1)
+list_images = df[df["max_multiple"] == 2]["image_id"].tolist()
+print(list_images)
