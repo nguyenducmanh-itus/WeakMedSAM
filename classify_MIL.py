6bb457df2b5542523980314cc92a945d8de2f413
Your Name <you@example.com>
2026-07-09 00:18:13 +0700

    Experimet for image extracted text final

--- classify_MIL.py
+++ classify_MIL.py
@@ -187,12 +187,11 @@ def train_and_extract_boxes(dir_img, current_epoch , pt_dir,
     optimizer.zero_grad()
     for n_iter in pbar :
         try : 
-            patches, label, _, image_path = next(train_loader_iter)
+            patches, label, _, _ = next(train_loader_iter)
         except :
             train_loader_iter = iter(train_dataloader)
-            patches, label, _, image_path = next(train_loader_iter)
-        
-        print(image_path)
+            patches, label, _, _ = next(train_loader_iter)
+            
         if patches.size(0) > MAX_PATCHES : 
             model.eval()
             with torch.no_grad() :
